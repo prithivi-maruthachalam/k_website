e1d735ffd07734ef62bee44e6cf7c27718f860d1
prythagoras <prithivimaruthachalam@gmail.com>
2021-04-02 21:38:25 +0530

    oi

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learnt React
+          Learnt React - oi
         </a>
       </header>
     </div>
