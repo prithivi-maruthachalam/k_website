6af43c044c4dd0e02eea00ac3a2b8b65af3b3928
prythagoras <prithivimaruthachalam@gmail.com>
2021-04-02 21:43:35 +0530

    Branch test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learnt React - oi
+          Learnt React - oi - prithivi's branch
         </a>
       </header>
     </div>
