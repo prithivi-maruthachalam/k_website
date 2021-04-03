982bee7b998dd66fb44b5e68eb1692b9fac5d1c4
prythagoras <prithivimaruthachalam@gmail.com>
2021-04-03 16:29:04 +0530

    Set up pages

---
+++ src/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+function Home() {
+  return (
+    <div className="fluid-contaniner">
+        Home
+      </div>
+  );
+}
+
+export default Home;
---
+++ src/page2.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+function Page2() {
+  return (
+    <div className="fluid-contaniner">
+        welcome to page 2
+      </div>
+  );
+}
+
+export default Page2;
